d07d7154cb2f972d1976663991bfe827c66f1246
Ilya Kolbasov <user@Users-MacBook-Pro.local>
2024-01-08 14:49:31 +0300

    feat:pom

--- src/main/java/by/koolbasov/videoperimetrproject/repository/UserRepository.java
+++ src/main/java/by/koolbasov/videoperimetrproject/repository/UserRepository.java
@@ -3,7 +3,7 @@ package by.koolbasov.videoperimetrproject.repository;
 import by.koolbasov.videoperimetrproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
